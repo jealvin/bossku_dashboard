325fda0c630402e39f2d39f90a189380671b62b1
Suriyadi <suryadi.surya5@gmail.com>
2018-10-03 00:37:16 +0700

    news dan forum

--- main.js
+++ main.js
@@ -32,89 +32,153 @@ function isValidAdmin(user,callback){
 }
 
 function appendMember(member,key){
-              var tr = "<tr memberId='" + key +"'><td>"+ member.firstName +"</td><td>"+ member.lastName +"</td><td>"+ member.emailAddress +"</td><td><a " + (member.membership_image ? "href='"+ (member.membership_image) +"'" : "") + "target='_blank'>Lihat Bukti</a></td><td><input type='checkbox' id='switcherySize2' class='switchery-sm' "+ (member.has_activate ? "checked":"") +"/></td></tr>";
+    var tr = "<tr memberId='" + key +"'><td>"+ member.firstName +"</td><td>"+ member.lastName +"</td><td>"+ member.emailAddress +"</td><td><a " + (member.membership_image ? "href='"+ (member.membership_image) +"'" : "") + "target='_blank'>Lihat Bukti</a></td><td><input type='checkbox' id='switcherySize2' class='switchery-sm' "+ (member.has_activate ? "checked":"") +"/></td></tr>";
 
-              var _tr = $(tr);
-              $("input[type=checkbox]",_tr).change(function(){
-                var isChecked = $(this).is(":checked");
-                firebase.database().ref('members/' + key + "/has_activate").set(isChecked); //set aktif atau nonaktif
-              });
+    var _tr = $(tr);
+    $("input[type=checkbox]",_tr).change(function(){
+      var isChecked = $(this).is(":checked");
+      firebase.database().ref('members/' + key + "/has_activate").set(isChecked); //set aktif atau nonaktif
+    });
 
-              var oldTr = $("tr[memberId="+ key +"]");
-              if(oldTr.length > 0){
-                oldTr.replaceWith(_tr);
-              }else{
-                $("#data-member").append(_tr);
-              }
-        };
+    var oldTr = $("tr[memberId="+ key +"]");
+    if(oldTr.length > 0){
+      oldTr.replaceWith(_tr);
+    }else{
+      $("#data-member").append(_tr);
+    }
+};
 
-        var memberRef = firebase.database().ref('members');
-        memberRef.on('child_added', function(data) {
-          appendMember( data.val(),data.key);
-        });
+var memberRef = firebase.database().ref('members');
+memberRef.on('child_added', function(data) {
+  appendMember( data.val(),data.key);
+});
 
-        memberRef.on('child_changed', function(data) {
-          appendMember( data.val(),data.key);
-        });
+memberRef.on('child_changed', function(data) {
+  appendMember( data.val(),data.key);
+});
 
-        memberRef.on('child_removed', function(data) {
-          var oldTr = $("tr[memberId="+ data.key +"]");
-          if(oldTr){
-            oldTr.remove();
-          }
-        });
+memberRef.on('child_removed', function(data) {
+  var oldTr = $("tr[memberId="+ data.key +"]");
+  if(oldTr){
+    oldTr.remove();
+  }
+});
+
+function deleteNews(key){
+  firebase.database().ref('articles/' + key).remove();
+};
+function deleteForum(key){
+  firebase.database().ref('topics/' + key).remove();
+};
+
 
 function appendArticle(article,key){
-              var tr = "<tr articleId='" + key +"'><td>"+ article.title +"</td></tr>";
-              var _tr = $(tr);
+    var tr = "<tr articleId='" + key +"'><td>"+ article.title +"</td></tr>";
+    var action = $("<td><a href='add-news.html?key=" + key + "' class='action-edit'>edit</a> | <a href='#' class='action-delete'>delete</a></td>");
+    $('.action-delete',action).click(function(){
+      bootbox.confirm("Are you sure?", function(result){ 
+        if(result){
+          deleteNews(key);
+        }
+      });
+    });
+    var _tr = $(tr);
+    _tr.append(action);
 
-              var oldTr = $("tr[articleId="+ key +"]");
-              if(oldTr.length > 0){
-                oldTr.replaceWith(_tr);
-              }else{
-                $("#data-news").append(_tr);
-              }
-        };
+    var oldTr = $("tr[articleId="+ key +"]");
+    if(oldTr.length > 0){
+      oldTr.replaceWith(_tr);
+    }else{
+      $("#data-news").prepend(_tr);
+    }
+};
 
-        var articleRef = firebase.database().ref('articles');
-          articleRef.on('child_added', function(data) {
-            appendArticle( data.val(),data.key);
-          });
+function appendForum(forum,key){
+    var tr = "<tr forumId='" + key +"'><td>"+ forum.title +"</td><td>" + (forum.open ? "true" : "false") + "</td></tr>";
+    var action = $("<td><a href='#' class='action-edit' data-toggle='modal' data-target='#forumModal' data-key='" + key + "'>edit</a> | <a href='#' class='action-delete'>delete</a></td>");
+    $('.action-delete',action).click(function(){
+      bootbox.confirm("Are you sure?", function(result){ 
+        if(result){
+          deleteForum(key);
+        }
+      });
+    });
+    var _tr = $(tr);
+    _tr.append(action);
 
-          articleRef.on('child_changed', function(data) {
-            appendArticle( data.val(),data.key);
-          });
+    var oldTr = $("tr[forumId="+ key +"]");
+    if(oldTr.length > 0){
+      oldTr.replaceWith(_tr);
+    }else{
+      $("#data-forum").prepend(_tr);
+    }
+};
 
-          articleRef.on('child_removed', function(data) {
-            var oldTr = $("tr[articleId="+ data.key +"]");
-            if(oldTr){
-              oldTr.remove();
+
+function save_article(articleId, id, title, content, urlImg, createdAt, updatedAt) {
+  bootbox.confirm("Are you sure?", function(result){ 
+    if(result){
+       var id = firebase.database().ref().child('articles').push().key;
+       var articleId = firebase.database().ref().child('articles').push().key;
+       var title = document.getElementById('title').value;
+       var content = document.getElementById('content').value;
+       var urlImg = document.getElementById('urlImg').src;
+       var createdAt = firebase.database.ServerValue.TIMESTAMP;
+       var updatedAt = firebase.database.ServerValue.TIMESTAMP;
+
+       var oldId = document.getElementById('articleKey').value;
+       if(oldId != null && oldId != ''){
+        articleId = oldId;
+       }
+       var oldImg = document.getElementById('oldImg').src;
+       if(oldImg != null && oldImg != ''){
+        urlImg = oldImg;
+       }
+
+       if (title == "" || content == "") {
+            alert("Title and content are required");
+            exit;
+       } 
+       else {
+        if(oldId != null && oldId != ''){
+          var updates = {};
+          updates['/articles/'+ articleId] = {
+            /*articleId:articleId,*/
+            /*id: id,*/
+            title: title,
+            content: content,
+            urlImg: urlImg,
+            /*createdAt: createdAt,*/
+            updatedAt: updatedAt
+         }
+          firebase.database().ref().update(updates, function(err){
+            if(err){
+              alert('failed save');
+            }else{
+              location.reload();
             }
           });
 
-function save_article(articleId, id, title, content, urlImg, createdAt, updatedAt) {
-   var id = firebase.database().ref().child('articles').push().key;
-   var articleId = firebase.database().ref().child('articles').push().key;
-   var title = document.getElementById('title').value;
-   var content = document.getElementById('content').value;
-   var urlImg = document.getElementById('urlImg').src;
-   var createdAt = firebase.database.ServerValue.TIMESTAMP;
-   var updatedAt = firebase.database.ServerValue.TIMESTAMP;
-
-   if (title == "" || content == "") {
-        alert("Title and content are required");
-        exit;
-   } 
-   else {
-    firebase.database().ref('articles/' + articleId).set({
-      articleId:articleId,
-      id: id,
-      title: title,
-      content: content,
-      urlImg: urlImg,
-      createdAt: createdAt,
-      updatedAt: updatedAt
-   });
-   }
+        }else{
+          firebase.database().ref('articles/' + articleId).set({
+            articleId:articleId,
+            id: id,
+            title: title,
+            content: content,
+            urlImg: urlImg,
+            createdAt: createdAt,
+            updatedAt: updatedAt
+         }, function(err){
+            if(err){
+              alert('failed save');
+            }else{
+              location.reload();
+            }
+          });
+        }
+       }
+      
+    }
+  });
    
 }
